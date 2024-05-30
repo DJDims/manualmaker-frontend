2f8e92e702e0de4e01e21e47a842cc224082e0a5
DJDims <djdims123@mail.ru>
2024-05-31 00:51:59 +0300

    Update

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,36 +3,61 @@ import { useCookies } from "react-cookie";
 import { IUser } from "../interfaces";
 import { useParams } from "react-router-dom";
 import ButtonLink from "./ButtonLink";
+import Button from "./Button";
+import axios from "axios";
+import { baseURL } from "../config";
 
 interface IProps {
 	user: IUser;
 }
 
-const Profile: React.FC<IProps> = ({user}) => {
-	const {userId} = useParams();
+const Profile: React.FC<IProps> = ({ user }) => {
+	const { userId } = useParams();
 	const [id, setId] = useState("");
-	const [cookies] = useCookies();
-	useEffect(()=>{
+	const [cookies, setCookies] = useCookies();
+	useEffect(() => {
 		if (!userId) {
-			setId(cookies.user._id)
+			setId(cookies.user._id);
 		} else {
-			setId(userId)
+			setId(userId);
 		}
-	}, [])
+	}, []);
+
+	const follow = async () =>{
+		const response = await axios.post(baseURL + "/users/follow/" + userId, {}, {
+			headers: {
+				token: cookies.token
+			}
+		})
+		setCookies('user', response.data);
+	}
+	const unfollow = async () =>{
+		const response = await axios.delete(baseURL + "/users/follow/" + userId, {
+			headers: {
+				token: cookies.token
+			}
+		})
+		setCookies('user', response.data);
+	}
+
 	return (
 		<>
 			<div className='user'>
 				<img src={user.avatar} alt='user avatar' />
 				<h3>{user.username}</h3>
 				<div className='followers'>
-					<a href={'/followers/'+id}>
-						{user.followers.length} followers
-					</a>
-					<a href={'/following/'+id}>
-						{user.following.length} following
-					</a>
+					<a href={"/followers/" + id}>{user.followers.length} followers</a>
+					<a href={"/following/" + id}>{user.following.length} following</a>
 				</div>
-				<ButtonLink label='Edit profile' color="default" path="/profile_edit"/>
+				{userId === cookies.user._id || userId === undefined ? (
+					<ButtonLink label='Edit profile' color='default' path='/profile_edit' />
+				) : (
+					cookies.user.following.includes(userId) ? (
+						<Button label='Unfollow' color='red' onButtonClick={unfollow}/>
+					) : (
+						<Button label='Follow' color='green' onButtonClick={follow}/>
+					)
+				) }
 			</div>
 		</>
 	);
